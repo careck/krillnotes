a55e07d82c5ec1890d7ca9e20f308b0d9e64db95
careck <careck@gmail.com>
2026-02-18 15:13:12 +1100

    chore: remove unused Window import

--- krillnotes-desktop/src-tauri/src/lib.rs
+++ krillnotes-desktop/src-tauri/src/lib.rs
@@ -9,7 +9,7 @@ use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
-use tauri::{AppHandle, Manager, State, Window};
+use tauri::{AppHandle, Manager, State};
 
 #[derive(Clone)]
 pub struct AppState {
